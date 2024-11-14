871dc58e64f9916da8c7f89ea73aeaf36451e490
CERN FUN <github-tmp@parisjug.org>
2024-11-14 11:28:21 +0100

    feat: set programme with user interact

--- checklist/Notes.md
+++ checklist/Notes.md
@@ -5,5 +5,5 @@
 - create tests for camera
 
 - create calibration test for camera+LED
-  - light 1.1 red, 1,16 green 16,1 blue 16,16 yellow
+  - light 1.1 red, 1,8 green 8,1 blue 8,8 yellow
   - check on camera (potentially adapt rotation)
--- checklist/neopixel/main.py
+++ checklist/neopixel/main.py
@@ -15,11 +15,30 @@ def fill_in_square(pixels):
         print("change led0")
         pixels.show()
 
+# This function lihght up the led board in green on by one with a pace of 0.05 sec
+def fill_led_with_calib_light(pixels):
+    pixels.fill( (0, 0, 0))
+    sleep(0.05)
+    pixels.show()
+    pixels[0]=(10,0,0)
+    pixels[7]=(10,10,0)
+    pixels[27]=(0,10,0)
+    pixels[28]=(0,0,10)
+    pixels[35]=(10,10,0)
+    pixels[36]=(10,0,0)
+    pixels[56]=(0,0,10)
+    pixels[63]=(0,10,0)
+    sleep(0.30)
 
 if __name__ == "__main__":
     pixels = neopixel.NeoPixel(board.D18, 64)
     print("init")
-    fill_in_square(pixels)
+    pixels.fill( (0, 0, 0))
+    display = input("select display [0,1] : ")
+    if (display == "0"):
+        fill_in_square(pixels)
+    if (display == "1"):
+        fill_led_with_calib_light(pixels)
     sleep(0.30)
     pixels.fill( (0, 0, 0))
     print("stop")
